1e4d10da17db36cd0f4e3645a44fbe318cf142dc
sidemt <25644062+sidemt@users.noreply.github.com>
2023-07-25 14:22:55 +0900

    Install Tailwind CSS

--- publish-frontend/src/pages/_app.js
+++ publish-frontend/src/pages/_app.js
@@ -1,11 +1,9 @@
-import '@/styles/globals.css'
-// import '@/styles/editor.css'
-import '@/styles/plain-text-editor.css'
-import { SessionProvider } from "next-auth/react"
+import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
 export default function App({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { session, ...pageProps }
 }) {
   return (
     <SessionProvider session={session}>
