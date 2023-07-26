9e6aaf19ce29e70a5a65c96d0a822967a184746a
sidemt <25644062+sidemt@users.noreply.github.com>
2023-07-26 11:19:33 +0900

    Add function to get user role

---
+++ publish-frontend/src/lib/users.js
@@ -0,0 +1,21 @@
+// Users & Permissions API calls
+
+const api_root = `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api`;
+
+export async function getMe(token) {
+  const endpoint = `${api_root}/users/me?populate=*`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+  const res = await fetch(endpoint, options);
+  if (!res.ok) {
+    throw new Error('getMe Failed');
+  }
+  return res.json();
+}
