a9473958202627c4b8a70bd11162a01ed78250c2
sembauke <semboot699@gmail.com>
2023-08-10 14:53:13 +0200

    fix: multiple issues

--- publish-frontend/src/pages/index.js
+++ publish-frontend/src/pages/index.js
@@ -1,11 +1,8 @@
-import { useEffect } from 'react';
-import Image from 'next/image';
 import NextLink from 'next/link'; // import as NextLink to avoid conflict with chakra-ui Link component
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { getMe } from '../lib/users';
+import { useSession, signIn } from 'next-auth/react';
 import { getArticles } from '../lib/articles';
 import NavMenu from '@/components/nav-menu';
-// Chakra UI components
+
 import { Button } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/react';
 import { Flex } from '@chakra-ui/react';
@@ -25,34 +22,42 @@ export default function IndexPage({ allArticlesData }) {
   const isLoading = status === 'loading';
   if (isLoading) return 'Loading...';
 
+  if (session) {
+    return (
+      <Flex>
+        <NavMenu session={session} />
+
+        <main className='p-3'>
+          <div>
+            <Button colorScheme='blue' as={NextLink} href='/articles/new'>
+              New Article
+            </Button>
+          </div>
+
+          <div>
+            <ul>
+              {allArticlesData.data.map(article => {
+                return (
+                  <li key={article.id} className='mb-5'>
+                    <Link as={NextLink} href={`/articles/${article.id}`}>
+                      <strong>{article.attributes.title}</strong>
+                      <br />
+                      {article.attributes.body.slice(0, 150)}...
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </main>
+      </Flex>
+    );
+  }
+
   return (
-    <Flex>
-     {/* <NavMenu session={session} /> */}
-
-      <main className='p-3'>
-        <div>
-          <Button colorScheme='blue' as={NextLink} href='/articles/new'>
-            New Article
-          </Button>
-        </div>
-
-        <div>
-          <ul>
-            {allArticlesData.data.map(article => {
-              return (
-                <li key={article.id} className='mb-5'>
-                  <Link as={NextLink} href={`/articles/${article.id}`}>
-                    <strong>{article.attributes.title}</strong>
-                    <br />
-                    {article.attributes.body.slice(0, 150)}...
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </main>
-    </Flex>
+    <>
+      Not signed in <br />
+      <Button onClick={() => signIn()}>Sign in</Button>
+    </>
   );
 }
-
