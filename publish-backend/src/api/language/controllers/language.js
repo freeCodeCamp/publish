807c2f7261a1bd86006db227ffb7e82b37f467e7
sidemt <25644062+sidemt@users.noreply.github.com>
2023-08-16 02:03:39 +0900

    fix: remove unused models

--- publish-backend/src/api/language/controllers/language.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * language controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::language.language');
--- publish-backend/src/api/language/services/language.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * language service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::language.language');
