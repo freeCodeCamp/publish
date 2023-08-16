ffbdede02388b0743cca955780cdf73172ec1eee
Niraj Nandish <nirajnandish@icloud.com>
2023-08-16 22:06:36 +0400

    chore: update sample.env

--- publish-backend/src/api/invited-user/content-types/invited-user/lifecycles.js
+++ publish-backend/src/api/invited-user/content-types/invited-user/lifecycles.js
@@ -2,11 +2,15 @@
 
 module.exports = {
   async afterCreate(event) {
+    const DASHBOARD_URL =
+      process.env.DASHBOARD_URL === ""
+        ? "http://localhost:3000"
+        : process.env.DASHBOARD_URL;
     await strapi.plugins.email.services.email.send({
       to: event.result.email,
       from: "support@freecodecamp.org",
       subject: "Invitation Link",
-      text: `Here is your invitation link: ${process.env.DASHBOARD_URL}/api/auth/signin`,
+      text: `Here is your invitation link: ${DASHBOARD_URL}/api/auth/signin`,
     });
   },
 };
