35f8228204351139170d73cc9a6f03bf779ecdaf
sidemt <25644062+sidemt@users.noreply.github.com>
2023-08-22 02:02:10 +0900

    feat(backend): handle populate option

--- publish-backend/src/api/custom-post/controllers/custom-post.js
+++ publish-backend/src/api/custom-post/controllers/custom-post.js
@@ -4,12 +4,33 @@
  * A set of functions called "actions" for `custom-post`
  */
 
+// Allow query parameter to be "authors" to comply with the old API
+// and convert it to "author" to use for populate option
+const authorsToAuthor = (populate) => {
+  const index = populate.indexOf("authors");
+  if (index !== -1) {
+    populate[index] = "author";
+  }
+  return populate;
+};
+
+// Sanitize the query parameter to only allow "authors" and "tags"
+const sanitizePopulate = (includeQuery) => {
+  const expectedValues = ["authors", "tags"];
+  const input = includeQuery?.split(",") || [];
+
+  // Filter out unexpected values and remove duplicates
+  const filtered = [...new Set(input.filter((item) => expectedValues.includes(item)))];
+  return authorsToAuthor(filtered);
+}
+
 module.exports = {
   find: async (ctx, next) => {
     try {
+      const populate = sanitizePopulate(ctx.request.query.include);
       const response = await strapi
         .service("api::custom-post.custom-post")
-        .find();
+        .find(populate);
       ctx.body = response;
     } catch (err) {
       ctx.body = err;
@@ -17,9 +38,10 @@ module.exports = {
   },
   findOne: async (ctx, next) => {
     try {
+      const populate = sanitizePopulate(ctx.request.query.include);
       const response = await strapi
         .service("api::custom-post.custom-post")
-        .findOne(ctx.request.params.id);
+        .findOne(ctx.request.params.id, populate);
       ctx.body = response;
     } catch (err) {
       ctx.body = err;
@@ -27,9 +49,10 @@ module.exports = {
   },
   findOneBySlug: async (ctx, next) => {
     try {
+      const populate = sanitizePopulate(ctx.request.query.include);
       const response = await strapi
         .service("api::custom-post.custom-post")
-        .findOneBySlug(ctx.request.params.slug);
+        .findOneBySlug(ctx.request.params.slug, populate);
       ctx.body = response;
     } catch (err) {
       ctx.body = err;
--- publish-backend/src/api/custom-post/services/custom-post.js
+++ publish-backend/src/api/custom-post/services/custom-post.js
@@ -1,58 +1,116 @@
 const { createCoreService } = require("@strapi/strapi").factories;
 
-// function to rename Strapi's default object keys
-const renamePostKeys = (postObj) => {
-  const { createdAt, updatedAt, publishedAt, ...rest } = postObj;
+// format data structure to match the old API
+// and remove attributes we don't want to include in the response
+const formatAuthor = (authorObj = {}) => {
+  const {
+    createdAt,
+    updatedAt,
+    // Exclude the attributes below from the response
+    username,
+    email,
+    provider,
+    password,
+    resetPasswordToken,
+    confirmationToken,
+    confirmed,
+    blocked,
+    // Exclude the attributes above from the response
+    ...rest
+  } = authorObj;
+
   return {
+    ...rest,
+    created_at: createdAt,
+    updated_at: updatedAt,
+  };
+};
+
+// format data structure to match the old API
+const formatTag = (tagObj = {}) => {
+  const { createdAt, updatedAt, ...rest } = tagObj;
+
+  return {
+    ...rest,
+    created_at: createdAt,
+    updated_at: updatedAt,
+  };
+};
+
+// format data structure to match the old API
+const formatPost = (postObj) => {
+  const { createdAt, updatedAt, publishedAt, author, tags, ...rest } = postObj;
+
+  let formattedPost = {
     ...rest,
     created_at: createdAt,
     updated_at: updatedAt,
     published_at: publishedAt,
   };
-}
+
+  console.log("author", author);
+  console.log("tags", tags);
+
+  if (author) {
+    const formattedAuthor = formatAuthor(author);
+    // Note: with current model relation, there is only 1 author for a post
+    // but to comply with the format of the old API we are putting it in an array
+    formattedPost.authors = [formattedAuthor];
+  }
+
+  if (tags) {
+    const formattedTags = tags.map((tag) => formatTag(tag));
+    formattedPost.tags = formattedTags;
+  }
+
+  return formattedPost;
+};
 
 module.exports = createCoreService("api::post.post", ({ strapi }) => ({
-  async find(...args) {
+  async find(populate = []) {
     const posts = await strapi.entityService.findMany("api::post.post", {
       publicationState: "live", // only published posts
       sort: { publishedAt: "desc" },
+      populate: populate,
     });
 
     // rename object keys
-    const renamedPosts = posts.map((post) => renamePostKeys(post));
+    const formattedPosts = posts.map((post) => formatPost(post));
 
     // format data into desired structure
-    const response = { posts: renamedPosts };
+    const response = { posts: formattedPosts };
     return response;
   },
 
-  async findOne(id, ...args) {
+  async findOne(id, populate = []) {
     const post = await strapi.entityService.findOne("api::post.post", id, {
       publicationState: "live", // only published posts
+      populate: populate,
     });
 
     // rename object keys
-    const renamedPost = renamePostKeys(post);
+    const formattedPost = formatPost(post);
 
     // format data into desired structure
-    const response = { posts: [renamedPost] };
+    const response = { posts: [formattedPost] };
     return response;
   },
 
-  async findOneBySlug(slug, ...args) {
+  async findOneBySlug(slug, populate = []) {
     console.log(slug);
     const posts = await strapi.entityService.findMany("api::post.post", {
+      publicationState: "live", // only published posts
       filters: {
         slug: slug,
       },
-      publicationState: "live", // only published posts
+      populate: populate,
     });
 
     // rename object keys
-    const renamedPosts = posts.map((post) => renamePostKeys(post));
+    const formattedPosts = posts.map((post) => formatPost(post));
 
     // format data into desired structure
-    const response = { posts: renamedPosts };
+    const response = { posts: formattedPosts };
     return response;
   },
 }));
