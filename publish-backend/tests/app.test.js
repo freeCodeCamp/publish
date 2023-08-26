5dafe055755cda550aeac5a96f5035713efd58e6
sidemt <25644062+sidemt@users.noreply.github.com>
2023-08-27 02:23:13 +0900

    feat(backend): set up testing environment

---
+++ publish-backend/tests/app.test.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const { setupStrapi, cleanupStrapi } = require("./helpers/strapi");
+
+beforeAll(async () => {
+  await setupStrapi();
+});
+
+afterAll(async () => {
+  await cleanupStrapi();
+});
+
+it("strapi is defined", () => {
+  expect(strapi).toBeDefined();
+});
+
+require("./user");
---
+++ publish-backend/tests/user/index.js
@@ -0,0 +1,32 @@
+const request = require("supertest");
+
+// user mock data
+const mockUserData = {
+  username: "tester",
+  email: "tester@strapi.com",
+  provider: "local",
+  password: "1234abc",
+  confirmed: true,
+  blocked: null,
+};
+
+it("should login user and return jwt token", async () => {
+  /** Creates a new user and save it to the database */
+  await strapi.plugins["users-permissions"].services.user.add({
+    ...mockUserData,
+  });
+
+  await request(strapi.server.httpServer) // app server is an instance of Class: http.Server
+    .post("/api/auth/local")
+    .set("accept", "application/json")
+    .set("Content-Type", "application/json")
+    .send({
+      identifier: mockUserData.email,
+      password: mockUserData.password,
+    })
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((data) => {
+      expect(data.body.jwt).toBeDefined();
+    });
+});
