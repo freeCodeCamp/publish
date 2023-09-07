772f6d9ce79e5a6e7b8feea0c5bd8b24874d63b3
sembauke <semboot699@gmail.com>
2023-09-07 12:42:48 +0200

    feat: introduce Chakra UI components

--- publish-frontend/src/components/post-form.jsx
+++ publish-frontend/src/components/post-form.jsx
@@ -3,24 +3,28 @@ import Tiptap from '@/components/tiptap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import slugify from 'slugify';
-import { Img } from '@chakra-ui/react';
-import { useSession } from 'next-auth/react';
+import {
+  Flex,
+  Img,
+  Box,
+  Button,
+  Text,
+  Select,
+  Input,
+  Spacer,
+  Stack,
+  FormControl,
+  FormErrorMessage
+} from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 import { createPost, updatePost } from '@/lib/posts';
 
 const PostForm = ({ tags, author, initialValues }) => {
-  const { data: session } = useSession();
-
-  const [showDrafts, setShowDrafts] = useState(true);
-  const [showPinned, setShowPinned] = useState(true);
-  const [showPublished, setShowPublished] = useState(true);
-
   const [title, setTitle] = useState('this is the title');
   const [isEditingTitle, setIsEditingTitle] = useState(false);
 
   const [clientTags, setClientTags] = useState([]);
   const [clientTagsId, setClientTagsId] = useState([]);
-
-  const [isFocused, setIsFocused] = useState(false);
 
   const [postUrl, setPostUrl] = useState('');
 
@@ -62,9 +66,12 @@ const PostForm = ({ tags, author, initialValues }) => {
       const { title, body } = initialValues.attributes;
       const { id } = initialValues;
 
+      console.log(initialValues);
+
       setTitle(title);
       setContent(body);
       setPostId(id);
+      // setClientTags();
     }
   }, [initialValues]);
 
@@ -73,16 +80,10 @@ const PostForm = ({ tags, author, initialValues }) => {
     setFeatureImage(URL.createObjectURL(file));
   }
 
-  function handleTitleChange() {
-    const newTitle = document.getElementById('title-input').value;
-    setTitle(newTitle);
-  }
-
   function addTag(event) {
     if (!clientTags.includes(event.target.value)) {
       const newTags = [...clientTags, event.target.value];
 
-      // compare the newTags array to the tags array and get the id of the new tag
       const newTagsInt = [];
       newTags.forEach(tag => {
         tags.forEach(t => {
@@ -106,15 +107,16 @@ const PostForm = ({ tags, author, initialValues }) => {
     const data = {
       data: {
         title: title,
-        slug: slugify(postUrl != '' ? postUrl : title, { lower: true }),
+        slug: slugify(postUrl != '' ? postUrl : title, {
+          lower: true,
+          specialChar: false
+        }),
         body: content,
         tags: clientTagsId,
         author: [author],
         locale: 'en'
       }
     };
-
-    console.log(data);
 
     try {
       if (!id) {
@@ -128,200 +130,202 @@ const PostForm = ({ tags, author, initialValues }) => {
     } catch (error) {
       console.log(error);
     }
-
-    console.log(data);
   };
 
   return (
-    <div className='page'>
-      <div className='side-drawer'>
-        {!isFocused ? (
-          <>
-            <button
-              className='dropdown-button'
-              onClick={() => setShowDrafts(!showDrafts)}
-            >
-              <h2>Drafts</h2>
-            </button>
-            {showDrafts && (
-              <div>
-                <ul>{}</ul>
-              </div>
-            )}
-
-            <button
-              className='dropdown-button'
-              onClick={() => setShowPinned(!showPinned)}
-            >
-              <h2>Pinned</h2>
-            </button>
-            {showPinned && (
-              <div>
-                <ul>
-                  <li>Post 4</li>
-                  <li>Post 5</li>
-                  <li>Post 6</li>
-                </ul>
-              </div>
-            )}
-
-            <button
-              className='dropdown-button'
-              onClick={() => setShowPublished(!showPublished)}
-            >
-              <h2>Published</h2>
-            </button>
-            {showPublished && (
-              <div>
-                <ul>
-                  <li>Post 7</li>
-                  <li>Post 8</li>
-                  <li>Post 9</li>
-                </ul>
-              </div>
-            )}
-
-            <button
-              className='submit-button full-width-btn'
-              onClick={() => handleSubmit(session)}
-            >
-              Save Draft
-            </button>
-          </>
-        ) : (
-          <div>
-            <h2 className='input-title'>Feature Image</h2>
-            <div className='feature-image'>
-              {featureImage ? (
-                <Img src={featureImage} alt='Feature Image' />
-              ) : (
-                <>
-                  <label htmlFor='feature-image' className='custom-file-upload'>
-                    <button
-                      type='button'
-                      onClick={() =>
-                        document.getElementById('feature-image').click()
-                      }
-                    >
-                      Select Image
-                    </button>
-                  </label>
-                  <input
-                    type='file'
-                    id='feature-image'
-                    accept='image/*'
-                    style={{ display: 'none' }}
-                    onChange={handleFileInputChange}
-                  />
-                </>
-              )}
-            </div>
-            {featureImage && (
-              <button
-                className='submit-button full-width-btn delete-button'
-                onClick={() => setFeatureImage(null)}
-              >
-                Delete
-              </button>
-            )}
-            <h2 className='input-title'>Tags</h2>
-            <div id='tag-container'></div>
-            <select className='tag-selector' onChange={addTag}>
-              <option value='0'>Select a tag</option>
-              {tags.map(tag => (
-                <option key={tag.attributes.name} value={tag.attributes.name}>
-                  {tag.attributes.name}
-                </option>
-              ))}
-            </select>
-            <h2 className='input-title'>Publish Date</h2>
-            <div className='time-date-input'>
-              <input
-                type='date'
-                id='publish-date'
-                name='publish-date'
-                required
-              />
-              <input
-                type='time'
-                id='publish-time'
-                name='publish-time'
-                required
-              />
-            </div>
-            <h2 className='input-title'>Post URL</h2>
-            <label htmlFor='slug'>
-              <input
-                type='text'
-                id='slug'
-                name='slug'
-                pattern='\S+'
-                placeholder={slugify(title)}
-                onChange={() => setPostUrl(event.target.value)}
-                required
-              />
-              <span>
-                https://www.freecodecamp.com/news/
-                {slugify(postUrl != '' ? postUrl : title, { lower: true })}
-              </span>
-            </label>
-            <button
-              className='submit-button full-width-btn'
-              onClick={() => handleSubmit(session)}
-            >
-              Save Draft
-            </button>
-            <br />
-            <hr />
-            <button
-              className='submit-button full-width-btn'
-              onClick={() => setIsFocused(false)}
-            >
-              View Posts
-            </button>
-          </div>
-        )}
-      </div>
-      <div className='post-container'>
-        <div className='header'>
-          <div className='title-pos' id='title'>
-            {isEditingTitle ? (
+    <Flex>
+      <Flex
+        flexDirection='column'
+        w={{ base: 'full', md: '300px' }}
+        h='100%'
+        bgColor='white'
+        borderRightWidth='1px'
+        padding={{ base: '0', md: '0.5rem' }}
+      >
+        <Box overflowY='scroll'>
+          <Box
+            size='lg'
+            py='1rem'
+            mx='20px'
+            textAlign='center'
+            fontWeight='700'
+            fontSize='20px'
+            display='flex'
+            alignItems='center'
+          >
+            <Img
+              src=' https://cdn.freecodecamp.org/platform/universal/fcc_puck_500.jpg'
+              width='32px'
+              height='32px'
+              mr='12px'
+              borderRadius='5px'
+            />
+            freeCodeCamp.org
+          </Box>
+          <Box
+            display='flex'
+            borderWidth='1px'
+            borderRadius='lg'
+            w='100%'
+            h='175px'
+            overflow='hidden'
+            bg='#f1f5f9'
+            justifyContent='center'
+            alignItems='center'
+          >
+            {!featureImage ? (
               <>
-                <input type='text' id='title-input' name='title-input' />
-
-                <button
-                  type='submit'
-                  className='submit-button icon-margin'
-                  onClick={() => {
-                    handleTitleChange();
-                    setIsEditingTitle(false);
-                  }}
-                >
-                  Save
-                </button>
+                <label htmlFor='feature-image' className='custom-file-upload'>
+                  <button
+                    type='button'
+                    onClick={() =>
+                      document.getElementById('feature-image').click()
+                    }
+                  >
+                    Select Image
+                  </button>
+                </label>
+                <input
+                  type='file'
+                  id='feature-image'
+                  accept='image/*'
+                  style={{ display: 'none' }}
+                  onChange={handleFileInputChange}
+                />{' '}
               </>
             ) : (
-              <button onClick={() => setIsEditingTitle(true)}>
-                <h1 className='title icon-margin-left'>
-                  <span>{title}</span>
-                  <FontAwesomeIcon icon={faEdit} />
-                </h1>
-              </button>
+              <Img
+                src={featureImage}
+                alt='feature'
+                borderRadius='lg'
+                objectFit='cover'
+                w='100%'
+                h='100%'
+              />
             )}
-          </div>
-          <div>
-            <button className='preview-button'>Preview</button>
-            <button className='submit-button'>Send for Review</button>
-          </div>
-        </div>
-        <div className='editor' onClick={() => setIsFocused(true)}>
+          </Box>
+          <Spacer h='1rem' />
+          {featureImage && (
+            <Button
+              colorScheme='red'
+              w='100%'
+              onClick={() => setFeatureImage(null)}
+            >
+              Delete Image
+            </Button>
+          )}
+          <Spacer h='1rem' />
+          <Text fontSize='xl'>Tags</Text>
+          <Box id='tag-container' display='flex' flexWrap='wrap' />
+          <Select
+            placeholder='Select option'
+            onChange={addTag}
+            w='100%'
+            marginTop='1rem'
+          >
+            {tags.map(tag => (
+              <option key={tag.id} value={tag.attributes.name}>
+                {tag.attributes.name}
+              </option>
+            ))}
+          </Select>
+          <Spacer h='1rem' />
+          <Text fontSize='xl'>Publish Date</Text>
+          <Box display='flex' flexDirection='row'>
+            <Input type='date' variant='outline' />
+            <Input type='time' variant='outline' />
+          </Box>
+          <Spacer h='1rem' />
+          <Text fontSize='xl'>Post Url</Text>
+          <label>
+            <Input
+              type='text'
+              placeholder='Post Url'
+              value={postUrl}
+              id='slug'
+              onChange={e => setPostUrl(e.target.value)}
+              w='100%'
+              marginTop='1rem'
+              variant='outline'
+            />
+            <Text fontStyle='italic' fontSize='md'>
+              https://www.freecodecamp.com/news/
+              {slugify(postUrl != '' ? postUrl : title, {
+                lower: true,
+                specialChar: false
+              })}
+            </Text>
+          </label>
+          <Spacer h='1rem' />
+          <Button colorScheme='blue' w='100%' onClick={() => handleSubmit}>
+            Save as Draft
+          </Button>
+        </Box>
+      </Flex>
+      <Flex flexDirection='column'>
+        <Flex marginTop='1rem' marginLeft='5rem'>
+          {!isEditingTitle ? (
+            <>
+              <Stack direction='row' onClick={() => setIsEditingTitle(true)}>
+                <Text fontSize='2xl'>{title}</Text>
+                <Text fontSize='2xl'>
+                  <FontAwesomeIcon icon={faEdit} />
+                </Text>
+              </Stack>
+            </>
+          ) : (
+            <Formik
+              initialValues={{ title: title }}
+              onSubmit={(values, actions) => {
+                setTitle(values.title);
+                setIsEditingTitle(false);
+                actions.setSubmitting(false);
+              }}
+            >
+              {props => (
+                <Form>
+                  <Stack direction='row'>
+                    <Field name='title'>
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={form.errors.title && form.touched.title}
+                        >
+                          <Input {...field} placeholder='title' />
+                          <FormErrorMessage>
+                            {form.errors.title}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button
+                      colorScheme='blue'
+                      isLoading={props.isSubmitting}
+                      type='submit'
+                    >
+                      Submit
+                    </Button>
+                  </Stack>
+                </Form>
+              )}
+            </Formik>
+          )}
+          <Stack direction='row' spacing={4} marginLeft='auto'>
+            <Button colorScheme='blue' variant='outline'>
+              Preview
+            </Button>
+            <Button colorScheme='blue' variant='solid'>
+              Send For Review
+            </Button>
+          </Stack>
+        </Flex>
+        <div className='editor'>
           <Tiptap
             handleContentChange={handleContentChange}
             defaultValue={content}
           />
         </div>
-      </div>
-    </div>
+      </Flex>
+    </Flex>
   );
 };
 export default PostForm;
