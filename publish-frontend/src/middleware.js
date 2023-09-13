ef827cbf22241de22b70ecc96ebb54d6dc3d41ee
Niraj Nandish <nirajnandish@icloud.com>
2023-09-13 19:38:35 +0400

    feat: restrict routes based on role

--- publish-frontend/src/middleware.js
+++ publish-frontend/src/middleware.js
@@ -1,4 +1,16 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(req => {
+  if (
+    req.nextUrl.pathname.startsWith('/tags') ||
+    req.nextUrl.pathname.startsWith('/users')
+  ) {
+    if (req.nextauth.token.userRole !== 'Editor') {
+      return NextResponse.redirect(new URL('/posts', req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: ['/posts/:path*', '/tags/:path*', '/users/:path*']
