de5e15dacfd9849de6ae45c6b604a2d2e64921a5
sidemt <25644062+sidemt@users.noreply.github.com>
2023-09-20 21:20:45 +0900

    feat(backend): edit comments

--- publish-backend/config/cron-tasks.js
+++ publish-backend/config/cron-tasks.js
@@ -9,18 +9,19 @@ module.exports = {
     const draftPostToPublish = await strapi.entityService.findMany(
       'api::post.post',
       {
-        publicationState: 'preview', // preview returns both draft and published entries
+        // there is no publicationState option to directly filter only draft posts
+        publicationState: 'preview', // 'preview' returns both draft and published posts
         filters: {
           publishedAt: {
-            $null: true, // so we add another condition here to filter entries that have not been published
+            $null: true, // filter posts that have not been published
           },
           publish_at: {
-            $lt: new Date(), // and we keep only posts with a 'publish_at' datetime value that is lower than the current datetime
+            $lt: new Date(), // AND 'publish_at' is lower than the current datetime
           },
         },
       }
     );
-    // update the publish_at of posts previously fetched
+    // publish the posts fetched above
     await Promise.all(
       draftPostToPublish.map((post) => {
         return strapi.entityService.update('api::post.post', post.id, {
