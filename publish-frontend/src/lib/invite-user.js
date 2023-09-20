75aed8c7e60f02945e45f036730559b5f7bdbbd3
Niraj Nandish <nirajnandish@icloud.com>
2023-09-20 23:51:28 +0400

    feat: basic users list

--- publish-frontend/src/lib/invite-user.js
+++ publish-frontend/src/lib/invite-user.js
@@ -1,5 +1,29 @@
 const api_root = `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api`;
 
+export async function getInvitedUsers(token) {
+  const res = await fetch(`${api_root}/invited-users`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  try {
+    if (!res.ok) {
+      throw new Error(
+        data.message || `Something went wrong. Status: ${res?.status}`
+      );
+    }
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error(`getInvitedUsers Failed. Error: `, error);
+    throw new Error(`getInvitedUsers Failed. Error: ${error}`);
+  }
+}
+
 export async function inviteUser(email, token) {
   const res = await fetch(`${api_root}/invited-users`, {
     method: 'POST',
--- publish-frontend/src/pages/users/index.js
+++ publish-frontend/src/pages/users/index.js
@@ -1,29 +1,70 @@
-import { getUsers } from '@/lib/users';
-import NavMenu from '@/components/nav-menu';
-import UsersList from '@/components/users-list';
-// Get session in getServerSideProps
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { Box, Button, Flex, Heading, Spacer, Text } from '@chakra-ui/react';
 import { getServerSession } from 'next-auth/next';
-import { Flex } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+import NavMenu from '@/components/nav-menu';
+import { getInvitedUsers } from '@/lib/invite-user';
+import { getUsers } from '@/lib/users';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
   const allUsersData = await getUsers(session.user.jwt);
+  const invitedUsers = await getInvitedUsers(session.user.jwt);
+
   return {
     props: {
       allUsersData,
+      invitedUsers,
       user: session.user
     }
   };
 }
 
-export default function UsersIndex({ allUsersData, user }) {
+export default function UsersIndex({ allUsersData, invitedUsers, user }) {
   return (
-    <Flex>
+    <Box minH='100vh' bgColor='gray.200'>
       <NavMenu user={user} />
-      <main style={{ padding: '0.75rem' }}>
-        <UsersList allUsersData={allUsersData} />
-      </main>
-    </Flex>
+
+      <Box ml={{ base: 0, md: '300px' }} px='6'>
+        <Flex
+          alignItems='center'
+          minH='20'
+          position={{ md: 'sticky' }}
+          top='0'
+          bgColor='gray.200'
+          zIndex='9999'
+        >
+          <Heading>Staff users</Heading>
+          <Spacer />
+          <Button colorScheme='blue' as={NextLink} href='/tags/new'>
+            Invite user
+          </Button>
+        </Flex>
+
+        <Box pb='10' mt='4'>
+          <Heading size='md'>Invited Users</Heading>
+          <Box py='4'>
+            {invitedUsers.data.map(user => {
+              if (user.attributes.accepted) return null;
+              return (
+                <Box key={user.id}>
+                  <Text>{user.attributes.email}</Text>
+                </Box>
+              );
+            })}
+          </Box>
+
+          <Heading size='md'>Active Users</Heading>
+          <Box py='4'>
+            {allUsersData.map(user => (
+              <Box key={user.id}>
+                <Text>{user.email}</Text>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
