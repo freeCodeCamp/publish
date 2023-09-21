e20e36efb20ff534f0eccd768719dfa904901e58
Niraj Nandish <nirajnandish@icloud.com>
2023-09-21 08:44:32 +0400

    style: user lists

--- publish-frontend/src/pages/users/index.js
+++ publish-frontend/src/pages/users/index.js
@@ -1,4 +1,14 @@
-import { Box, Button, Flex, Heading, Spacer, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Spacer,
+  Text
+} from '@chakra-ui/react';
+import intlFormatDistance from 'date-fns/intlFormatDistance';
 import { getServerSession } from 'next-auth/next';
 import NextLink from 'next/link';
 
@@ -9,19 +19,19 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
-  const allUsersData = await getUsers(session.user.jwt);
+  const allUsers = await getUsers(session.user.jwt);
   const invitedUsers = await getInvitedUsers(session.user.jwt);
 
   return {
     props: {
-      allUsersData,
+      allUsers,
       invitedUsers,
       user: session.user
     }
   };
 }
 
-export default function UsersIndex({ allUsersData, invitedUsers, user }) {
+export default function UsersIndex({ allUsers, invitedUsers, user }) {
   return (
     <Box minH='100vh' bgColor='gray.200'>
       <NavMenu user={user} />
@@ -44,24 +54,78 @@ export default function UsersIndex({ allUsersData, invitedUsers, user }) {
 
         <Box pb='10' mt='4'>
           <Heading size='md'>Invited Users</Heading>
-          <Box py='4'>
+          <Box my='4' boxShadow='md' bgColor='white'>
             {invitedUsers.data.map(user => {
               if (user.attributes.accepted) return null;
+
+              const userEmail = user.attributes.email;
+              const createdAt = intlFormatDistance(
+                new Date(user.attributes.createdAt),
+                new Date()
+              );
               return (
-                <Box key={user.id}>
-                  <Text>{user.attributes.email}</Text>
-                </Box>
+                <Flex
+                  key={user.id}
+                  py='2'
+                  px='4'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  flexWrap={{ base: 'wrap', md: 'nowrap' }}
+                  rowGap='2'
+                  borderBottom='1px solid'
+                  borderColor='gray.200'
+                >
+                  <Flex alignItems='center' pr='2'>
+                    <Avatar size='sm' mr='4' />
+                    <Flex flexDirection='column'>
+                      <Text fontWeight='600'>{userEmail}</Text>
+                      <Text fontSize='sm' color='gray.500'>
+                        Invitation sent: {createdAt}
+                      </Text>
+                    </Flex>
+                  </Flex>
+                  <Flex
+                    alignItems='center'
+                    fontSize='sm'
+                    columnGap='4'
+                    fontWeight='500'
+                  >
+                    <Text>Revoke</Text>
+                    <Text>Resend</Text>
+                    <Badge>Editor</Badge>
+                  </Flex>
+                </Flex>
               );
             })}
           </Box>
 
           <Heading size='md'>Active Users</Heading>
-          <Box py='4'>
-            {allUsersData.map(user => (
-              <Box key={user.id}>
-                <Text>{user.email}</Text>
-              </Box>
-            ))}
+          <Box my='4' boxShadow='md' bgColor='white'>
+            {allUsers.map(user => {
+              const userEmail = user.email;
+              console.log(user);
+              const userRole = user.role.name;
+              return (
+                <Flex
+                  key={user.id}
+                  py='2'
+                  px='4'
+                  alignItems='center'
+                  justifyContent='space-between'
+                  flexWrap={{ base: 'wrap', md: 'nowrap' }}
+                  rowGap='2'
+                  borderBottom='1px solid'
+                  borderColor='gray.200'
+                  cursor='pointer'
+                >
+                  <Flex alignItems='center' pr='2'>
+                    <Avatar size='sm' mr='4' />
+                    <Text fontWeight='600'>{userEmail}</Text>
+                  </Flex>
+                  <Badge>{userRole}</Badge>
+                </Flex>
+              );
+            })}
           </Box>
         </Box>
       </Box>
