e1244962affd36771e4bca15ced90bf8a415b044
Muhammed Mustafa <MuhammedElruby@gmail.com>
2023-10-01 19:21:37 +0300

    feat: stop the header from stacking on top of the sideMenu (#227)
    Co-authored-by: yoko <25644062+sidemt@users.noreply.github.com>

--- publish-frontend/src/pages/posts/index.js
+++ publish-frontend/src/pages/posts/index.js
@@ -44,6 +44,7 @@ const FilterButton = ({ text }) => {
       borderRadius='md'
       fontSize='14px'
       boxShadow='sm'
+      position='unset'
       _hover={{
         boxShadow: 'md'
       }}
@@ -117,7 +118,6 @@ export default function IndexPage({ posts, users, tags, user }) {
           position='sticky'
           top='0'
           bgColor='gray.200'
-          zIndex='9999'
         >
           <Heading>Posts</Heading>
           <Spacer />
