b39324f8a3b85f7151830692ee7b82f8d2c836cf
Niraj Nandish <nirajnandish@icloud.com>
2023-10-01 22:06:33 +0400

    feat: allow contributers to edit their profile

--- publish-frontend/src/middleware.js
+++ publish-frontend/src/middleware.js
@@ -1,11 +1,43 @@
 import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { NextResponse, URLPattern } from 'next/server';
+
+const PATTERNS = [
+  [
+    new URLPattern({ pathname: '/users/:id' }),
+    ({ pathname }) => pathname.groups
+  ]
+];
+
+const params = url => {
+  const input = url.split('?')[0];
+  let result = {};
+
+  for (const [pattern, handler] of PATTERNS) {
+    const patternResult = pattern.exec(input);
+    if (patternResult !== null && 'pathname' in patternResult) {
+      result = handler(patternResult);
+      break;
+    }
+  }
+  return result;
+};
 
 export default withAuth(req => {
-  if (
-    req.nextUrl.pathname.startsWith('/tags') ||
-    req.nextUrl.pathname.startsWith('/users')
-  ) {
+  if (req.nextUrl.pathname.startsWith('/users')) {
+    const { id } = params(req.url);
+    if (req.nextauth.token.userRole !== 'Editor') {
+      if (id !== undefined) {
+        if (req.nextauth.token.id != id) {
+          return NextResponse.redirect(new URL('/posts', req.url));
+        }
+      } else {
+        return NextResponse.redirect(new URL('/posts', req.url));
+      }
+    }
+  }
+  if (req.nextUrl.pathname.startsWith('/tags')) {
+    const { id } = params(req.url);
+    console.log('id', id);
     if (req.nextauth.token.userRole !== 'Editor') {
       return NextResponse.redirect(new URL('/posts', req.url));
     }
