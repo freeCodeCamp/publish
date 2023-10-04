a9578524bc19fed1de797dd2f975b9b45264bb5a
sembauke <semboot699@gmail.com>
2023-10-04 15:24:42 +0200

    fix: injecting new tweets

--- publish-frontend/src/components/twitter-embed.jsx
+++ publish-frontend/src/components/twitter-embed.jsx
@@ -2,12 +2,8 @@ import { NodeViewWrapper } from '@tiptap/react';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 export default function TwitterEmbed(props) {
-  const hasTweetId = props.node.attrs.tweetId !== null;
-
-  const tweetIds = hasTweetId ? props.node.attrs.tweetId.split('/') : [];
-  const lastTweetId =
-    tweetIds.length > 0 ? tweetIds[tweetIds.length - 1] : '1709136269995872521';
-
+  const tweetIds = props.node.attrs.tweetId.split('/');
+  const lastTweetId = tweetIds[tweetIds.length - 1];
   return (
     <NodeViewWrapper className='twitter-embed'>
       <TwitterTweetEmbed tweetId={lastTweetId} />
--- publish-frontend/tiptap-extensions/src/embed.js
+++ publish-frontend/tiptap-extensions/src/embed.js
@@ -11,6 +11,9 @@ export const Embed = Node.create({
       tweetId: {
         default: null
       },
+      // a private value that stores the children before they are renedered in the dom
+      // if this is not done, the React Component will be injected and show an iframe instead.
+
       storeChildren: {
         default: null
       }
@@ -25,12 +28,25 @@ export const Embed = Node.create({
           class: 'kg-card kg-embed-card'
         },
         getAttrs: document => {
+          // This makes sure that the already injected tweet is parsed correctly
+
+          // TODO: check if the last anchor element always contains the tweet id (probably not)
           const anchorElements = document.getElementsByTagName('a');
+          console.log(document + 'document');
+          if (anchorElements.length === 0) {
+            return {
+              storeChildren: document
+            };
+          }
+
           const lastAnchorElement = anchorElements[anchorElements.length - 1];
           const href = lastAnchorElement.getAttribute('href');
 
           const endOfTweetPath = href.split('/');
           const tweetId = endOfTweetPath[endOfTweetPath.length - 1];
+
+          // check if the tweet path contains any keys that need to be
+          // removed before returning a tweed id
 
           if (tweetId.includes('?')) {
             const tweetIdSplit = tweetId.split('?');
@@ -50,18 +66,28 @@ export const Embed = Node.create({
   },
 
   renderHTML({ HTMLAttributes }) {
+    // the dom (element) represents the root of theh node
+
     const dom = document.createElement('figure');
     dom.setAttribute('class', 'kg-card kg-embed-card');
+
+    // the contentDOM (element) represents the child of the dom element
 
     const contentDOM = document.createElement('blockqoute');
     contentDOM.setAttribute('class', 'twitter-tweet');
     contentDOM.setAttribute('data-width', '550');
 
-    Array.from(HTMLAttributes.storeChildren?.children).forEach(child => {
-      if (child.nodeName !== 'SCRIPT') {
-        contentDOM.appendChild(child);
-      }
-    });
+    // the storeChildren attribute contains the children of the node before they are rendered in the dom
+    console.log(HTMLAttributes.storeChildren);
+    if (HTMLAttributes.storeChildren) {
+      Array.from(HTMLAttributes.storeChildren.children).forEach(child => {
+        if (child.nodeName !== 'SCRIPT') {
+          contentDOM.appendChild(child);
+        }
+      });
+    }
+
+    // now we make sure that the contentDOM is a child of the dom element
 
     dom.appendChild(contentDOM);
 
