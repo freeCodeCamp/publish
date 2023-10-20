8bec7727cc5d21c4b5f9f4798ef2e9eb89827b50
Niraj Nandish <nirajnandish@icloud.com>
2023-10-20 18:40:25 +0400

    chore: delete strapi cron task files

--- publish-backend/config/server.js
+++ publish-backend/config/server.js
@@ -1,5 +1,3 @@
-// const cronTasks = require('./cron-tasks');
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -9,8 +7,4 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  // cron: {
-  //   enabled: true,
-  //   tasks: cronTasks,
-  // },
 });
