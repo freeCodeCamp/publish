0b6bc06c6b3dbc81a69cf46926c8c6ade77b473b
Niraj Nandish <nirajnandish@icloud.com>
2023-10-20 17:27:16 +0400

    feat: publish service

---
+++ publish-service/src/index.ts
@@ -0,0 +1,18 @@
+import Bree from 'bree';
+import path from 'path';
+
+const bree = new Bree({
+  root: path.join(__dirname, 'jobs'),
+  defaultExtension: process.env.NODE_ENV === 'production' ? 'js' : 'ts',
+  jobs: [
+    {
+      name: 'check-and-publish',
+      interval: '5m',
+      timeout: 0,
+    },
+  ],
+});
+
+(async () => {
+  await bree.start();
+})();
