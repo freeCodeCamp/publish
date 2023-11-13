fbe215f7eaa8113c6aa6fdbdce2b532817b03709
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-11-13 10:58:47 +0100

    fix: update db config (dev + prod) (#327)

--- apps/backend/config/env/production/database.js
+++ apps/backend/config/env/production/database.js
@@ -2,12 +2,20 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
+      connectionString: env("DATABASE_URL"),
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "password"),
-      ssl: env.bool("DATABASE_SSL", false),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        key: env("DATABASE_SSL_KEY", undefined),
+        cert: env("DATABASE_SSL_CERT", undefined),
+        ca: env("DATABASE_SSL_CA", undefined),
+        capath: env("DATABASE_SSL_CAPATH", undefined),
+        cipher: env("DATABASE_SSL_CIPHER", undefined),
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
+      },
     },
     dialect: "postgres",
   },
