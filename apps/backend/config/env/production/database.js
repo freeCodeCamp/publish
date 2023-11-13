b1492ab04756470ea8dbce1f5964652342277887
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-11-13 10:51:27 +0100

    feat: allow connection via connection strings

--- apps/backend/config/env/production/database.js
+++ apps/backend/config/env/production/database.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
+      connectionString: env("DATABASE_URL"),
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
