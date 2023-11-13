1e5af8fbad09cb82bd1ed2da8ade1aa4d57ed0e9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-11-13 10:48:23 +0100

    chore: remove unused dev db config

--- apps/backend/config/env/development/database.js
+++ apps/backend/config/env/development/database.js
@@ -1,63 +1,12 @@
-// To use SQLite for testing and PostgreSQL for development,
-// this file has to be in /config/env / development directory.
-// If database.js is present under /config directory, values from that file
-// affect the test environment setup and result in an error.
-
-const path = require("path");
+// To use PostgreSQL for development, this file has to be in /config/env /
+// development directory. If database.js is present under /config directory,
+// values from that file affect the test environment setup and result in an
+// error.
 
 module.exports = ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
   const connections = {
-    mysql: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true,
-          ),
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    mysql2: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true,
-          ),
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
@@ -83,16 +32,6 @@ module.exports = ({ env }) => {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", "data.db"),
-        ),
-      },
-      useNullAsDefault: true,
     },
   };
 
