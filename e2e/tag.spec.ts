27418ebb11d769775b3a4e948d005c07d061654d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-11-29 18:07:28 +0100

    test: ensure the browser navigates back to /tags

--- e2e/tag.spec.ts
+++ e2e/tag.spec.ts
@@ -1,8 +1,15 @@
-import { expect, test } from "@playwright/test";
+import { type Page, expect, test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
     await page.goto("/tags");
 });
+
+// Helper function to wait for the tags page to load. This is because waitForURL
+// doesn't always work as expected. However, we know that the "New Tag" button
+// is always visible on the tags page.
+async function waitForTags(page: Page) {
+    return await expect(page.getByRole("link", { name: "New Tag" })).toBeVisible();
+}
 
 test("it should be possible to create and delete a new tag", async ({ page }) => {
     // first create a new tag
@@ -12,7 +19,7 @@ test("it should be possible to create and delete a new tag", async ({ page }) =>
     await page.fill("input[name=slug]", "my-new-tag");
 
     await page.getByRole("button", { name: "Save" }).click();
-    await page.waitForURL("**/tags");
+    await waitForTags(page)
     
     // now delete it
     await page.getByText("My new tag").click();
@@ -29,8 +36,7 @@ test("it should be possible to edit a tag", async ({ page }) => {
 
     await page.fill("input[name=name]", "HTML - edited");
     await page.getByRole("button", { name: "Save" }).click();
-    await page.waitForURL("**/tags");
-
+    await waitForTags(page)
     await expect(page.getByText("HTML - edited")).toBeVisible();
     
     // undo the edit
@@ -48,8 +54,7 @@ test("it should be possible to create a new internal tag", async ({ page }) => {
     await page.check('text=Internal Tag');
 
     await page.getByRole("button", { name: "Save" }).click();
-
-    await page.getByText("Internal Tags").click()
+    await page.getByRole('radiogroup').getByText('internal tags').click()
 
     // TODO: delete via api call and just check that it's visible
     await page.getByText("My new tag").click();
