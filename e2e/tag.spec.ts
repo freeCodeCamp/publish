0f93b15d88b3a738a438045f19e4c9d2134a372f
sembauke <semboot699@gmail.com>
2023-11-29 11:33:15 +0100

    feat: create normal tag test

--- e2e/tag.spec.ts
+++ e2e/tag.spec.ts
@@ -1,23 +1,42 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
+
+test("it should be possible to create a new tag", async ({ page }) => {
+    await page.goto("/tags");
+    
+    await page.getByRole("link", { name: "New Tag" }).click();
+
+    await page.fill("input[name=name]", "My new tag 1");
+    await page.fill("input[name=slug]", "my-new-tag-1");
+
+    await page.getByRole("button", { name: "Save" }).click();
+
+    await page.waitForTimeout(1000);
+
+    await page.goto("/tags");
+
+    await page.getByText("My new tag 1").isVisible();
+});
 
 test("it should be possible to create a new internal tag", async ({ page }) => {
     await page.goto("/tags");
     
     await page.getByRole("link", { name: "New Tag" }).click();
 
-    await page.fill("input[name=name]", "My new tag");
-    await page.fill("input[name=slug]", "my-new-tag");
+    await page.fill("input[name=name]", "My new tag 2");
+    await page.fill("input[name=slug]", "my-new-tag-2");
 
     await page.check('text=Internal Tag');
 
     await page.getByRole("button", { name: "Save" }).click();
 
-    await page.waitForSelector("text=Tag Created.");
+    await page.waitForTimeout(1000);
 
     await page.goto("/tags?visibility=internal&page=1");
 
-    await page.getByText("My new tag").isVisible();
+    await page.getByText("My new tag 2").isVisible();
 });
+
+
 
 
 
