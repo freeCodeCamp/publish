94c83d052d5e44a878fcb689175f251f66cbca58
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-12-04 14:49:28 +0100

    test(e2e): user becomes active on signin (#359)
    Co-authored-by: yoko <25644062+sidemt@users.noreply.github.com>

---
+++ e2e/helpers/constants.ts
@@ -0,0 +1,16 @@
+export const API_URL = "http://localhost:1337";
+
+export const EDITOR_CREDENTIALS = {
+  identifier: "editor@user.com",
+  password: "editor",
+};
+
+export const INVITEE_CREDENTIALS = {
+  identifier: "invited@user.com",
+  password: "invited",
+};
+
+export const CONTRIBUTOR_CREDENTIALS = {
+  identifier: "contributor@user.com",
+  password: "contributor",
+};
