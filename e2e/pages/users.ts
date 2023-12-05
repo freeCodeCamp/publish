51efef9a23ed3dbca1b829ce0d8f031cfbabf6cd
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-12-05 14:51:27 +0100

    test: improve and cleanup invitation tests (#368)
    Co-authored-by: yoko <25644062+sidemt@users.noreply.github.com>

---
+++ e2e/pages/users.ts
@@ -0,0 +1,38 @@
+import type { Page, Locator } from "@playwright/test";
+
+export class UsersPage {
+  private readonly activeUsers: Locator;
+  private readonly invitedUsers: Locator;
+
+  constructor(public readonly page: Page) {
+    this.activeUsers = page.locator('[data-testid="active-user"]');
+    this.invitedUsers = page.locator('[data-testid="invited-user"]');
+  }
+
+  async getInvitedUser(email: string) {
+    return this.invitedUsers.filter({ hasText: email });
+  }
+
+  async getActiveUser(email: string) {
+    return this.activeUsers.filter({ hasText: email });
+  }
+
+  async inviteUser(email: string) {
+    await this.page.getByRole("button", { name: "Invite user" }).click();
+    await this.page.getByLabel("Email*").click();
+    await this.page.getByLabel("Email*").fill(email);
+    await this.page.getByRole("button", { name: "Send invitation" }).click();
+  }
+
+  async revokeUser(email: string) {
+    const revokeButton = (await this.getInvitedUser(email)).getByRole(
+      "button",
+      { name: "Revoke" }
+    );
+    await revokeButton.click();
+  }
+
+  async goto() {
+    await this.page.goto("/users");
+  }
+}
