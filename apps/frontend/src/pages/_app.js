b31e1ece04e7ac5c38a84330ea3353150da73812
Niraj Nandish <nirajnandish@icloud.com>
2024-01-28 22:37:33 +0530

    chore: cleanup import order

--- apps/frontend/src/pages/_app.js
+++ apps/frontend/src/pages/_app.js
@@ -1,13 +1,12 @@
-import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import { SessionProvider } from "next-auth/react";
 
 import theme from "@/lib/theme";
 
-// import Font Awesome CSS
+import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import { config } from "@fortawesome/fontawesome-svg-core";
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
 config.autoAddCss = false;
