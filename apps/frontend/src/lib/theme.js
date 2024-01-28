b281ff1431a77251e6218111996c0dd9b0685d2f
Niraj Nandish <nirajnandish@icloud.com>
2024-01-28 22:32:10 +0530

    chore: combine theme config

--- apps/frontend/src/lib/theme.js
+++ apps/frontend/src/lib/theme.js
@@ -1,10 +1,25 @@
 import { extendTheme } from "@chakra-ui/react";
+import { withProse } from "@nikolovlazar/chakra-ui-prose";
 
 const config = {
   initialColorMode: "system",
   useSystemColorMode: false,
+  components: {
+    Table: {
+      variants: {
+        simple: {
+          th: {
+            borderColor: "gray.200",
+          },
+          td: {
+            borderColor: "gray.200",
+          },
+        },
+      },
+    },
+  },
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme(config, withProse());
 
 export default theme;
--- apps/frontend/src/pages/_app.js
+++ apps/frontend/src/pages/_app.js
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { withProse } from "@nikolovlazar/chakra-ui-prose";
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
+
+import theme from "@/lib/theme";
 
 // import Font Awesome CSS
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -10,25 +11,6 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
 config.autoAddCss = false;
-const theme = extendTheme(
-  {
-    components: {
-      Table: {
-        variants: {
-          simple: {
-            th: {
-              borderColor: "gray.200",
-            },
-            td: {
-              borderColor: "gray.200",
-            },
-          },
-        },
-      },
-    },
-  },
-  withProse(),
-);
 
 export default function App({
   Component,
