6b9a36fdda59df6e6a5bb2ec393cba66eea3eb54
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-01-31 17:06:20 +0100

    chore: disable type-aware linting

--- e2e/.eslintrc.js
+++ e2e/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
+    // "plugin:@typescript-eslint/recommended-type-checked", // Disabled to
+    // allow us to incrementally fix the errors
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
