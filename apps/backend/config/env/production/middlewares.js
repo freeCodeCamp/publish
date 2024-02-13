d3a589f3bc643c11e61c2823fddaff779bbf3a35
sidemt <25644062+sidemt@users.noreply.github.com>
2024-02-14 06:08:59 +0900

    fix: show thumbnails in Strapi's Media Library

---
+++ apps/backend/config/env/production/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::body",
+  "strapi::public",
+];
